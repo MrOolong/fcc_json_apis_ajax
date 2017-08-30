28c011ee4aff8bfd69a46da0f20963cf679bcc46
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-30 15:20:05 +0000

    Trigger Click Events with jQuery

---
+++ json1.js
@@ -0,0 +1,9 @@
+//Trigger Click Events with jQuery
+
+  $(document).ready(function() {
+    // Only change code below this line.
+    $("#getMessage").on("click", function(){
+
+});
+    // Only change code above this line.
+  });
