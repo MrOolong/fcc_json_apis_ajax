2c86170ac4dee086042032993e75be3a40a75822
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-31 22:53:05 +0000

    Get Geolocation Data

---
+++ json7.js
@@ -0,0 +1,7 @@
+//Get Geolocation Data
+
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
