b0e2a37091dbe49bd54ab6228c916b145041bdfe
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-31 22:19:45 +0000

    Get JSON with the jQuery getJSON Method

---
+++ json3.js
@@ -0,0 +1,15 @@
+//Get JSON with the jQuery getJSON Method
+
+  $(document).ready(function() {
+
+    $("#getMessage").on("click", function(){
+      // Only change code below this line.
+      
+      $.getJSON("/json/cats.json", function(json) {
+  $(".message").html(JSON.stringify(json));
+});
+      
+      // Only change code above this line.
+    });
+
+  });
